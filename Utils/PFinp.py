a333bb3d5110d3b31dcc09834549b7b7626a77c2
marilu dick-perez <marilu@iastate.edu>
2016-06-23 16:43:40 -0500

    Change functions/variable names to be more accurate/descriptive

--- Utils/PFinp.py
+++ Utils/PFinp.py
@@ -12,28 +12,29 @@
 #
 
 ####################################################################################
+
 # --- Functions ---
-def PES_coordinate():
+def GetPESCoords():
     print("The following prompts define the variables used to generate" +
           "the QM PESs.")
-    atom_indices = raw_input("Enter atom indices separated by a space.\n")
+    AtomIndices = raw_input("Enter atom indices separated by a space.\n")
     i_coord = raw_input("Enter the initial length or angle.\n")
     f_coord = raw_input("Enter the final length or angle.\n")
     coord_s = raw_input("Enter the lenght or angle step size.\n")
-    return "{0}, {1} {2} {3}".format(atom_indices, i_coord, f_coord, coord_s)
+    return "{0}, {1} {2} {3}".format(AtomIndices, i_coord, f_coord, coord_s)
 
-def quantumdata(qmdatachoice):
-    if (qmdatachoice == "a" or qmdatachoice == ""):
-        qmdata = 'comp'
+def GetQMFileFormat(QMFormatChoice):
+    if (QMFormatChoice == "a" or QMFormatChoice == ""):
+        QMFormat = 'comp'
         print "Selected compact file; one file contains the QM data."
-        filenameroot = raw_input("Enter the root filename. ")
-    elif (qmdatachoice == "b"):
-        qmdata = 'full'
+        RootFilename = raw_input("Enter the root filename. ")
+    elif (QMFormatChoice == "b"):
+        QMFormat = 'full'
         print "Selected full; a series of files contain QM data."
-        filenameroot = raw_input("Enter the root filename. ")
-    return qmdata, filenameroot
+        RootFilename = raw_input("Enter the root filename. ")
+    return QMFormat, RootFilename
 
-def qmdata_prompt():
+def PromptQMFileFormat():
     print("<< QM data file format >>\n" +
           "(a) Compact: QM data is contained in one file that includes fixed\n" +
           "    bond lengths, bond angles, or torsion angle geometries.\n" +
@@ -41,21 +42,21 @@ def qmdata_prompt():
           "    bond angle, or torsion angle geometry.\n" +
           "Enter: a or b. [a]")
 
-def parameter_lines(PEStype, no_PESs):
-    if PEStype == "diha":
-        no_of_parameter_lines = no_PESs
-        p_list_len = no_of_parameter_lines*4
-        list_params = [None] * p_list_len
-        for m in range(0, p_list_len, 4):
-            list_params[m]= raw_input("Enter parameter line number for the dihedral to be fit. ")
+def GetParameterLines(VariedCoord, NoOfVariedCoords):
+    if VariedCoord == "diha":
+        NoOfParamLines = NoOfVariedCoords
+        ParamListLen = NoOfParamLines*4
+        ParamList = [None] * ParamListLen
+        for m in range(0, ParamListLen, 4):
+            ParamList[m]= raw_input("Enter parameter line number for the dihedral to be fit. ")
             for i in range(1, 4):
-                c_or_p = raw_input("Enter 'p' if Line " + list_params[m] + " V" +
+                c_or_p = raw_input("Enter 'p' if Line " + ParamList[m] + " V" +
                         str(i) + " should be varied during ParFit run. ")
                 if c_or_p == "p":
                     c_or_p = "p"
                 else:
                     c_or_p = "c"
-                list_params[m + i] = c_or_p
+                ParamList[m + i] = c_or_p
         i = raw_input("How many pairs of parameters are coupled? [0] ")
         if i == "":
             i = 0
@@ -67,31 +68,31 @@ def parameter_lines(PEStype, no_PESs):
                   "\t[line number] [line number] [parameter number] ")
         for n in range(i):
             q, r, s = str.split(raw_input("Enter the line and parameter numbers. "))
-            q_index = int(list_params.index(q))
-            r_index = int(list_params.index(r))
+            q_index = int(ParamList.index(q))
+            r_index = int(ParamList.index(r))
             s = int(s)
-            list_params[q_index + s] = list_params[r_index + s] = "p" + str(n + 1)
+            ParamList[q_index + s] = ParamList[r_index + s] = "p" + str(n + 1)
     else:
         m = raw_input("Enter parameter line number of the parameters to be fit. ")
-        list_params = m + " p p"
-#        print list_params  #print for debugging
-    return list_params
+        ParamList = m + " p p"
+#        print ParamList  #print for debugging
+    return ParamList
 ###################################################################################
 ###################################################################################
 
 # --- Determine ParFit input file name ---
 print("Enter the name of ParFit input file to create, if blank, the file " +
       "name will be PFinput.\n")
-pyout = raw_input()
-if (pyout == ""):
-    pyout = "PFinput"
-    print "[PFinp]: Input filename:", pyout, "\n"
+OutFileName = raw_input()
+if (OutFileName == ""):
+    OutFileName = "PFinput"
+    print "[PFinp]: Input filename:", OutFileName, "\n"
 else:
-    pyout == pyout
-    print "[PFinp]: Input filename:", pyout, "\n"
+    OutFileName == OutFileName
+    print "[PFinp]: Input filename:", OutFileName, "\n"
 
 # --- Open the file for writing ---
-f = open(pyout,'w')
+f = open(OutFileName,'w')
 
 # --- Select the parameter type, bond length, bond angle, torsion. ---
 property_type = raw_input('''Choose from the properties below:
@@ -102,43 +103,43 @@ property_type = raw_input('''Choose from the properties below:
 Enter: a, b, or c.\n''')
 
 if (property_type == "a"):
-    property_type = 'bond'
+    VariedCoord = 'bond'
     parameterize = "bond length"
 elif (property_type == "b"):
-    property_type = 'angl'
+    VariedCoord = 'angl'
     parameterize = "bond angle"
 elif (property_type == "c"):
-    property_type = 'diha'
+    VariedCoord = 'diha'
     parameterize = "torsion angle"
 else:
     print "The default, torsion angle, was chosen."
-    property_type = 'diha'
+    VariedCoord = 'diha'
     parameterize = "torsion angle"
 
 # --- Multiple dihedral angle file fitting. ---
-if (property_type == "bond" or property_type == "angl"):
-    qmdata_prompt()
-    qm_f_properties = quantumdata(qmdatachoice = raw_input())
-    no_PESs = 1
-#elif (property_type == "angl"):
-#    qmdata_prompt()
-#    qm_f_properties = quantumdata(qmdatachoice = raw_input())
-#    no_PESs = 1
-elif (property_type == "diha"):
-    no_PESs = int(raw_input("Enter the number of PESs to be fit.\n"))
-    for n in range(0, no_PESs):
-        qmdata_prompt()
-        qm_f_properties =  quantumdata(qmdatachoice = raw_input())
-        print qm_f_properties
-        if (qm_f_properties[0] != "comp"):
-            PES_properties = PES_coordinate()
+if (VariedCoord == "bond" or VariedCoord == "angl"):
+    PromptQMFileFormat()
+    QMFileProperties = GetQMFileFormat(QMFormatChoice = raw_input())
+    NoOfVariedCoords = 1
+#elif (VariedCoord == "angl"):
+#    PromptQMFileFormat()
+#    QMFileProperties = GetQMFileFormat(QMFormatChoice = raw_input())
+#    NoOfVariedCoords = 1
+elif (VariedCoord == "diha"):
+    NoOfVariedCoords = int(raw_input("Enter the number of PESs to be fit.\n"))
+    for n in range(0, NoOfVariedCoords):
+        PromptQMFileFormat()
+        QMFileProperties =  GetQMFileFormat(QMFormatChoice = raw_input())
+        print QMFileProperties
+        if (QMFileProperties[0] != "comp"):
+            PES_properties = GetPESCoords()
 
 # if not running a bond length/angle run, use this path.
 else:
     print "[PFinp] Error: You have not properly chosen a property to parameterize."
 
 # --- Determine which parameters will be changed by ParFit ---
-list_params = parameter_lines(property_type, no_PESs)
+ParamList = GetParameterLines(VariedCoord, NoOfVariedCoords)
 
 # --- Get engine path ---
 engine_path = raw_input("\nWhat is the full engine.exe path?\n")
@@ -180,25 +181,25 @@ else:
     csv = "csv_on"
 
 # --- Print out input file ---
-if (property_type == "diha"):
-    print >> f, "mult, " + str(no_PESs)
-    for i in range(no_PESs):
-        print >> f, ", ".join(qm_f_properties) + ", " + property_type
+if (VariedCoord == "diha"):
+    print >> f, "mult, " + str(NoOfVariedCoords)
+    for i in range(NoOfVariedCoords):
+        print >> f, ", ".join(QMFileProperties) + ", " + VariedCoord
 else:
-    print >> f, ", ".join(qm_f_properties) + ", " + property_type
+    print >> f, ", ".join(QMFileProperties) + ", " + VariedCoord
 print >> f, engine_path
 print >> f, mmtyp
 print >> f, alg
-if property_type == "diha":
-    for i in range(0, no_PESs*4, 4):
+if VariedCoord == "diha":
+    for i in range(0, NoOfVariedCoords*4, 4):
         n = i + 4
-        print >> f, " ".join(list_params[i:n])
+        print >> f, " ".join(ParamList[i:n])
 else:
-    print >> f, list_params
+    print >> f, ParamList
 #else:                 #print for debugging
-#    print list_params  #print for debugging
+#    print ParamList  #print for debugging
 print >> f, csv
 
-print "\nYour ParFit input file name {0} has been generated.\n".format(pyout)
+print "\nYour ParFit input file name {0} has been generated.\n".format(OutFileName)
 
 exit()
